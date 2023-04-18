57ffab5a7e1a89394d626e03b8c326ce7d8d9cda
nagyb3 <nagybence2003@gmail.com>
2023-04-18 22:59:11 +0200

    currentScore, highscore and images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Card from "./components/Card"
+import fox from "./images/fox.png"
+import elephant from "./images/elephant.png"
+import horse from "./images/horse.png"
+import koala from "./images/koala.png"
+import mosquito from "./images/mosquito.png"
+import rhino from "./images/rhino.png"
+import snake from "./images/snake.png"
+import zebra from "./images/zebra.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentScore, setCurrentScore] = React.useState(0);
+
+    const [highscore, setHighscore] = React.useState(0);
+
+    const [cardTouch, setCardTouch] = React.useState([false, false, false, false,
+        false, false, false, false, false])
+
+    const images = [fox, elephant, horse, koala, mosquito, rhino, snake, zebra]
+
+    function renderCards() {
+        return images.map(name => <Card image={name}/>)
+    }
+
+    return (
+        <div>
+            <header>
+                <p>current score: {currentScore}</p>
+                <p>highscore: {highscore}</p>
+                <hr/>
+            </header>
+            <div className="container">
+                {renderCards()}
+            </div>
+        </div>
+    );
 }
 
 export default App;
